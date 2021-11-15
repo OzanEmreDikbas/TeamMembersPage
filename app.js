2a41f9f19da9770c17a4088d4db740732d57ad3d
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-11-15 19:14:34 +0300

    sonlandı.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const bgrColorBtn = document.querySelector('button')
+const switchButton = document.querySelector('#switch');
 
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
-console.log(bgrColorBtn);
-bgrColorBtn.addEventListener('click', myFunction)
+console.log(switchButton);
+switchButton.addEventListener('click', myFunction);
